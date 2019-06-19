bbbc7933992c311f7e5d06786f28854c88e9dad6
Matthew Kling <mattkling@gmail.com>
2019-06-19 16:53:51 -0700

    add support for arbitrary directions

--- code/transects.R
+++ code/transects.R
@@ -11,6 +11,12 @@
 # forward, reverse windfilling
 
 
+library(windscape)
+library(raster)
+library(tidyverse)
+library(geosphere)
+library(grid)
+library(gridExtra)
 
 
 land <- raster("f:/cfsr/land.tif") %>% 
@@ -38,29 +44,46 @@ names(climate) <- c("clim0", "clim1")
 
 
 
-transect_plot <- function(transect = NULL){
+transect_plot <- function(transect = NULL, n=25){
       
       if(is.null(transect)){
             message("Click the map twice to draw an EAST-WEST transect, then hit ESC.")
-            plot(land)
+            plot(climate[[1]], 
+                 col=colorRampPalette(c("gray98" "cyan", "dodgerblue", "darkorchid", 
+                                        "red", "yellow"))(100))
             transect <- drawLine()
       }
-      tsr <- rasterize(transect, land) %>% trim()
       
-      clim <- climate %>% crop(tsr) %>% mask(tsr)
-      ros <- rose %>% crop(tsr) %>% mask(tsr)
+      coords <- coordinates(transect)[[1]][[1]]
+      tpoints <- apply(coords, 2, function(x) seq(x[1], x[2], length.out=n))
+      dst <- distGeo(tpoints[1,], tpoints) / 1000
       
-      d <- stack(clim, ros) %>%
-            rasterToPoints() %>%
+      
+      ros <- rose %>% 
+            subset(c("N", "NE", "E", "SE", "S", "SW", "W", "NW")) %>%
+            stack(., .) %>%
+            raster::extract(tpoints, method="bilinear")
+      clim <- raster::extract(climate, tpoints, method="bilinear")
+      
+      
+      bearing <- bearingRhumb(coords[1,], coords[2,])
+      octant <- floor(bearing / 45) + 1
+      remainder <- bearing %% 45
+      
+      fx <- function(x) sum(x * c(remainder, 45-remainder))/45
+      tw <- ros[, c(octant, octant+1)] %>% apply(1, fx)
+      hw <- ros[, c(octant+4, octant+5)] %>% apply(1, fx)
+      w <- tw - hw
+      
+      d <- cbind(dst, clim, w) %>%
             as.data.frame() %>%
             filter(!is.na(clim0)) %>%
-            group_by(x) %>%
-            slice(1) %>%
-            ungroup()
-      #res <- resolution(d$x)
-      mx <- diff(range(na.omit(d$x))) / 5
-      mn <- mx / 5
+            mutate(uclim = 1/(lag(clim0) - lead(clim0)),
+                   uwind = w)
       
+      
+      mx <- diff(range(na.omit(d$dst))) / 5
+      mn <- mx / 5
       norm <- function(x){
             x <- x / max(abs(x), na.rm=T) * mx
             inc <- is.finite(x) & abs(x) < mn
@@ -69,34 +92,28 @@ transect_plot <- function(transect = NULL){
       }
       
       d <- d %>% 
-            arrange(x) %>%
-            mutate(uwind = E+(SE+NE)*sqrt(2) - (W+(SW+NW)*sqrt(2)),
-                   uclim = 1/(lag(clim0) - lead(clim0)),
-                   
-                   uclim = sign(uclim) * scales::rescale(log(abs(uclim)), c(.01, 1)),
-                   
-                   uwind = norm(uwind),
-                   uclim = norm(uclim),
-                   
-                   uwind = ifelse(is.na(uclim), NA, uwind))
+            arrange(dst) %>%
+            mutate(uclim = sign(uclim) * scales::rescale(log(abs(uclim)), c(.01, 1)),
+                  uwind = norm(uwind),
+                  uclim = norm(uclim),
+                  uwind = ifelse(is.na(uclim), NA, uwind))
       
       arw <- arrow(type="closed", angle=15, length = unit(0.1, "inches"))
-      
-      ynudge <- 0#diff(range(d$clim0, na.rm=T))*.01
+      ynudge <- 0
       
       wind <- ggplot(d) +
-            geom_ribbon(aes(x, ymin=clim0, ymax=max(clim0 + 1, na.rm=T)), 
+            geom_ribbon(aes(dst, ymin=clim0, ymax=max(clim0 + 1, na.rm=T)), 
                         stat="identity", fill="gray90") +
-            geom_point(aes(x, clim0, color=sign(uwind)==sign(uclim)), size=4) +
-            scale_color_manual(values=c("darkred", "forestgreen")) +
-            geom_segment(aes(x=x, xend=x+uwind,
+            geom_segment(aes(x=dst, xend=dst+uwind,
                              y=clim0+ynudge, yend=clim0+ynudge),
                          arrow=arw, color="dodgerblue") +
-            geom_segment(aes(x=x, xend=x+uclim,
+            geom_segment(aes(x=dst, xend=dst+uclim,
                              y=clim0-ynudge, yend=clim0-ynudge),
                          arrow=arw, color="black") +
+            geom_point(aes(dst, clim0, color=sign(uwind)==sign(uclim)), size=4) +
+            scale_color_manual(values=c("darkred", "forestgreen")) +
             annotate(geom="text", 
-                     x=range(d$x, na.rm=T) + diff(range(d$x, na.rm=T)) / 20 * c(1,-1), 
+                     x=range(d$dst, na.rm=T) + diff(range(d$dst, na.rm=T)) / 20 * c(1,-1), 
                      y=max(d$clim0 + .5), hjust=c(0, 1),
                      label=c("climate tracking", "prevailing wind"), 
                      color=c("black", "dodgerblue")) +
@@ -104,9 +121,15 @@ transect_plot <- function(transect = NULL){
             theme(legend.position="none") +
             scale_y_reverse() +
             labs(y = "temperature (deg C)", 
-                 x = "longitude (deg)")
+                 x = "distance along transect (km)")
+      print(wind)
       
-      ##
+      return(list(transect = transect,
+           data = d,
+           wind = wind))
+      
+      ####################
+      # deprecated analog plot code below -- fix
       
       tol <- 2
       for(i in 1:nrow(d)){
@@ -162,8 +185,7 @@ transect_plot <- function(transect = NULL){
            analogs = analogs)
 }
 
-p <- transect_plot(p$transect)
-p$wind
+transect_plot()
 
 
 
