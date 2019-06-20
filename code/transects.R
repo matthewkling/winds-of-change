fce928cec7d8b2bb927c8fa2dccf6162118c556d
Matthew Kling <mattkling@gmail.com>
2019-06-20 14:34:09 -0700

    new examples and key map

--- code/transects.R
+++ code/transects.R
@@ -61,6 +61,7 @@ transect_plot <- function(transect = NULL, n=25){
       
       coords <- coordinates(transect)[[1]][[1]]
       tpoints <- apply(coords, 2, function(x) seq(x[1], x[2], length.out=n))
+      colnames(tpoints) <- c("x", "y")
       dst <- distGeo(tpoints[1,], tpoints) / 1000
       
       
@@ -75,12 +76,18 @@ transect_plot <- function(transect = NULL, n=25){
       octant <- floor(bearing / 45) + 1
       remainder <- bearing %% 45
       
+      # net wind in transect direction
       fx <- function(x) sum(x * c(remainder, 45-remainder))/45
       tw <- ros[, c(octant, octant+1)] %>% apply(1, fx)
       hw <- ros[, c(octant+4, octant+5)] %>% apply(1, fx)
       w <- tw - hw
       
-      d <- cbind(dst, clim, w) %>%
+      total <- ros[,1:8] %>% apply(1, sum)
+      prop <- (tw + hw) / total
+      message(mean(prop, na.rm=T))
+      
+      
+      d <- cbind(dst, clim, w, tpoints) %>%
             as.data.frame() %>%
             filter(!is.na(clim0)) %>%
             mutate(uclim = 1/(lag(clim0) - lead(clim0)),
@@ -99,9 +106,11 @@ transect_plot <- function(transect = NULL, n=25){
       d <- d %>% 
             arrange(dst) %>%
             mutate(uclim = sign(uclim) * scales::rescale(log(abs(uclim)), c(.01, 1)),
-                  uwind = norm(uwind),
-                  uclim = norm(uclim),
-                  uwind = ifelse(is.na(uclim), NA, uwind))
+                   uwind = norm(uwind),
+                   uclim = norm(uclim),
+                   uwind = ifelse(is.na(uclim), NA, uwind),
+                   aligned = sign(uwind)==sign(uclim),
+                   aligned = factor(aligned, levels=c(T, F)))
       
       arw <- arrow(type="closed", angle=15, length = unit(0.1, "inches"))
       ynudge <- 0
@@ -115,8 +124,8 @@ transect_plot <- function(transect = NULL, n=25){
             geom_segment(aes(x=dst, xend=dst+uclim,
                              y=clim0-ynudge, yend=clim0-ynudge),
                          arrow=arw, color="black") +
-            geom_point(aes(dst, clim0, color=sign(uwind)==sign(uclim)), size=4) +
-            scale_color_manual(values=c("darkred", "forestgreen")) +
+            geom_point(aes(dst, clim0, color=aligned), size=4) +
+            scale_color_manual(values=c("forestgreen", "darkred"), drop=F) +
             annotate(geom="text", 
                      x=range(d$dst, na.rm=T) + diff(range(d$dst, na.rm=T)) / 20 * c(1,-1), 
                      y=max(d$clim0 + .5), hjust=c(0, 1),
@@ -130,8 +139,9 @@ transect_plot <- function(transect = NULL, n=25){
       print(wind)
       
       return(list(transect = transect,
-           data = d,
-           wind = wind))
+                  data = d,
+                  prop = prop,
+                  wind = wind))
       
       ####################
       # deprecated analog plot code below -- fix
@@ -190,24 +200,97 @@ transect_plot <- function(transect = NULL, n=25){
            analogs = analogs)
 }
 
-transect_plot()
+#tp <- transect_plot(tp$transect)
 
 
+##################################
 
-add_tag <- function(x, tag){
-      x$wind <- x$wind +
-            annotate(geom="text", label=tag,
-                     x=min(x$data$x, na.rm=T),
-                     y=min(x$data$clim0, na.rm=T),
-                     hjust=0, size=6)
-      x
-}
+australia <- transect_plot(australia$transect)
+greenland <- transect_plot(greenland$transect)
+rockies <- transect_plot(rockies$transect)
+cali <- transect_plot(cali$transect)
+russia <- transect_plot(russia$transect)
+sahara <- transect_plot(sahara$transect)
 
-# good ones
-brz <- transect_plot(brz$transect) %>% add_tag("\nBrazilian\ncerrado")
-aus <- transect_plot(aus$transect) %>% add_tag("\nAustralian\ncontinent")
-app <- transect_plot(app$transect) %>% add_tag("\nSouthern\nAppalachia")
-bc <- transect_plot(bc$transect) %>% add_tag("\nBritish\nColumbia")
+d <- list(rockies, cali, greenland, 
+          australia, russia, sahara)
 
-p <- arrangeGrob(bc$wind, brz$wind, app$wind, aus$wind, ncol=2)
-ggsave("figures/transects/transects.png", p, width=12, height=9, units="in")
+saveRDS(d, "data/transect_data.rds")
+d <- readRDS("data/transect_data.rds")
+
+labels <- c("Mountain range (American Rockies)", "(California)", "Ice cap (Greenland)",
+            "Desert (Australian Outback)", "Boreal plain (Russia)", "Thermal low (West Africa)")
+for(i in 1:length(d)) d[[i]]$data$id <- labels[i]
+
+d <- lapply(d, function(x) x$data) %>%
+      bind_rows() %>%
+      mutate(id=factor(id, levels=labels))
+
+d <- d %>%
+      group_by(id) %>%
+      mutate(ymax = max(clim0) + .1 * diff(range(clim0, na.rm=T))) %>%
+      mutate(i = as.integer(id),
+             idi = paste0("[", i, "] ", id))
+
+ynugde <- 0
+arw <- arrow(type="closed", angle=15, length = unit(0.05, "inches"))
+p <- ggplot(d) +
+      facet_wrap(~idi, scales="free") +
+      geom_ribbon(aes(dst, ymin=clim0, ymax=ymax), 
+                  stat="identity", fill="gray90") +
+      geom_point(aes(dst, clim0, color=aligned), size=2) +
+      geom_segment(aes(x=dst, xend=dst+uwind,
+                       y=clim0+ynudge, yend=clim0+ynudge, color=aligned),
+                   arrow=arw, #color="blue", 
+                   size=.3) +
+      # geom_segment(aes(x=dst, xend=dst+uclim,
+      #                  y=clim0-ynudge, yend=clim0-ynudge),
+      #              arrow=arw, color="black") +
+      scale_color_manual(values=c("forestgreen", "red"), drop=F) +
+      # annotate(geom="text", 
+      #          x=range(d$dst, na.rm=T) + diff(range(d$dst, na.rm=T)) / 20 * c(1,-1), 
+      #          y=max(d$clim0 + .5), hjust=c(0, 1),
+      #          label=c("climate tracking", "prevailing wind"), 
+      #          color=c("black", "dodgerblue")) +
+      theme_minimal() +
+      theme(legend.position="none") +
+      scale_y_reverse() +
+      labs(y = "temperature (°C; note flipped scale)", 
+           x = "distance along transect (km)")
+#ggsave("figures/transects/transects.png", p, width=8, height=6, units="in")
+
+
+climate <- list.files("data/cfsr_monthly/tmp2m", full.names=T)[1:24] %>%
+      stack() %>%
+      mean() %>%
+      rotate() %>%
+      mask(land) %>%
+      rasterToPoints() %>%
+      as.data.frame()
+
+
+dp <- d %>% group_by(id) %>% arrange(dst) %>% slice(1)
+#world <- map_data("world")
+map <- ggplot() +
+      #geom_polygon(data=world, aes(long, lat, group=group), fill="gray80") +
+      geom_raster(data=climate, aes(x, y, fill = layer)) +
+      geom_path(data=d, aes(x, y, group=id), color="black", size=.35,
+                arrow=arrow(type="closed", angle=20, length=unit(.05, "in"))) +
+      geom_point(data=dp, aes(x, y), color="black") +
+      geom_text(data=dp, aes(x, y-5, label=i), color="black", size=4) +
+      # scale_fill_gradientn(colors=c("gray97", "azure", "cyan", "dodgerblue", "blue",
+      #                               "purple", "red", "orange", "yellow")) +
+      scale_fill_gradientn(colors=c("white", "#e6e6ff", "#e6e6ff", "darkmagenta", "#6b49ff", 
+                                    "dodgerblue", "turquoise", "#48c13f", 
+                                    "yellow", "red", "#660000")) +
+      theme_void() +
+      theme(legend.position="none") +
+      scale_x_continuous(expand=c(0,0)) +
+      scale_y_continuous(expand=c(0,0), limits=c(-90, 90))
+#ggsave("figures/transects/map.png", p, width=6, height=3, units="in")
+
+
+pc <- arrangeGrob(p, map, ncol=1, heights=c(6, 4))
+ggsave("figures/transects/transects.png", pc, width=8, height=10, units="in")
+
+
