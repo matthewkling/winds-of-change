3bd0b716f4110241dca46c504e53e452c66a05ae
Matthew Kling <mattkling@gmail.com>
2019-06-26 22:54:27 -0700

    extent to multiple scales

--- code/headwinds_02_analysis.R
+++ code/headwinds_02_analysis.R
@@ -14,9 +14,9 @@ mf <- list.files(modir, full.names=T, recursive=T)
 
 
 # long-term means
-t <- mf[grepl("tmp2m", mf)] %>% stack() %>% mean()
-u <- mf[grepl("_u", mf)] %>% stack() %>% mean()
-v <- mf[grepl("_v", mf)] %>% stack() %>% mean()
+t <- mf[grepl("tmp2m", mf)][1:24] %>% stack() %>% mean()
+u <- mf[grepl("_u", mf)][1:72] %>% stack() %>% mean()
+v <- mf[grepl("_v", mf)][1:72] %>% stack() %>% mean()
 
 
 # convert wind components to speed and direction
@@ -30,99 +30,190 @@ wa[] <- w$azimuth
 gradient <- function(x, component="x"){
       y_cellsize <- 0.8 # guessing maybe PRISM quotes latitude as its raster size
       x_cellsize <- y_cellsize * 78846.81 / 111131.75 # this is the ratio at 45N
+      browser()
       dzdx <- ((x[3] + 2*x[6] + x[9]) - (x[1] + 2*x[4] + x[7])) / (8 * x_cellsize)
       dzdy <- ((x[7] + 2*x[8] + x[9]) - (x[1] + 2*x[2] + x[3])) / (8 * y_cellsize)
       if(component=="x"){return(dzdx)}
       if(component=="y"){return(dzdy)}
 }
-vx1 <- rotate(-1 * focal(t, w=matrix(1,3,3), fun=function(x) gradient(x, component="x")))
-vx2 <- -1 * focal(rotate(t), w=matrix(1,3,3), fun=function(x) gradient(x, component="x"))
-vx <- merge(vx1, vx2)
-vy1 <- rotate(focal(t, w=matrix(1,3,3), fun=function(x) gradient(x, component="y")))
-vy2 <- focal(rotate(t), w=matrix(1,3,3), fun=function(x) gradient(x, component="y"))
-vy <- merge(vy1, vy2)
-vx <- trim(vx)
-vy <- crop(vy, vx)
-tv <- MagnitudeAzimuth(values(vx), values(vy))
-tm <- ta <- vx
-tm[] <- log10(1 / tv$magnitude)
-ta[] <- tv$azimuth
 
-cr <- function(x) crop(rotate(x), tm)
-s <- stack(cr(u), cr(v), cr(wa), cr(wm), 
-           vx, vy, ta, tm)
-names(s) <- c("wx", "wy", "wa", "wm", 
-              "tx", "ty", "ta", "tm")
-
-land <- raster("f:/cfsr/land.tif")
-s <- stack(s, cr(land))
+grangle <- function(x){
+      n <- sqrt(length(x))
+      if(is.na(x[(length(x)+1)/2])) return(NA)
+      x <- matrix(x, n)
+      m <- lm(y ~ x, data = data.frame(y = apply(x, 1, mean, na.rm=T), x = (1:n)))
+      dx <- coef(m)["x"]
+      m <- lm(y ~ x, data = data.frame(y = apply(x, 2, mean, na.rm=T), x = (1:n)))
+      dy <- coef(m)["x"]
+      MagnitudeAzimuth(dx, dy)["azimuth"]
+}
 
 
+neighborhoods <- c(3, 9, 27)
+for(neigh in neighborhoods){
+      
+      wmat <- matrix(1, neigh, neigh)
+      
+      ta <- focal(mask(t, land), w=wmat, fun=grangle) %>% rotate()
+      taa <- focal(mask(t, land) %>% rotate(), w=wmat, fun=grangle)
+      ta <- merge(ta, taa)
+      
+      
+      # vx1 <- rotate(-1 * focal(t, w=wmat, fun=function(x) gradient(x, component="x")))
+      # vx2 <- -1 * focal(rotate(t), w=wmat, fun=function(x) gradient(x, component="x"))
+      # vx <- merge(vx1, vx2)
+      # vy1 <- rotate(focal(t, w=wmat, fun=function(x) gradient(x, component="y")))
+      # vy2 <- focal(rotate(t), w=wmat, fun=function(x) gradient(x, component="y"))
+      # vy <- merge(vy1, vy2)
+      # vx <- trim(vx)
+      # vy <- crop(vy, vx)
+      # tv <- MagnitudeAzimuth(values(vx), values(vy))
+      # tm <- ta <- vx
+      # tm[] <- log10(1 / tv$magnitude)
+      # ta[] <- tv$azimuth
+      
+      cr <- function(x) crop(rotate(x), ta)
+      s <- stack(cr(u), cr(v), cr(wa), cr(wm), 
+                 ta) #vx, vy, ta, tm)
+      names(s) <- c("wx", "wy", "wa", "wm", 
+                    "ta")# "tx", "ty", "ta", "tm")
+      
+      land <- raster("f:/cfsr/land.tif")
+      s <- stack(s, cr(land))
+      
+      
+      
+      ###########
+      
+      # project to hobo-dyer equal area
+      #s <- projectRaster(s, crs=CRS("+proj=cea +lon_0=0 +lat_ts=37.5 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs"))
+      
+      #continents <- map_data("world")
+      #states <- map_data("state")
+      
+      
+      ########################
+      
+      
+      d <- s %>% 
+            rasterToPoints %>% 
+            as.data.frame()
+      d <- d %>%
+            mutate(theta = abs(wa - ta),
+                   theta = ifelse(theta > 180, 360-theta, theta))
+      
+      d$tm <- 1
+      
+      dl <- filter(d, is.finite(land))
+      
+      
+      
+      compass <- ggplot() +
+            geom_point(data=data.frame(x=0:360), 
+                       aes(x, .9, color=x),
+                       size=5) +
+            geom_segment(data=data.frame(x=seq(0, 360, 45)), 
+                         aes(x=x, xend=x, y=.5, yend=1.25, color=x),
+                         arrow=arrow(type="closed", angle=12,
+                                     length=unit(0.5, "inches")),
+                         size=2) +
+            coord_polar() +
+            scale_x_continuous(breaks=seq(0, 360, 45),
+                               limits=c(0, 360)) +
+            scale_color_gradientn(colors=rainbow(20)) +
+            theme_minimal() +
+            theme(legend.position="none",
+                  axis.text.y=element_blank(),
+                  axis.text.x=element_blank(),
+                  panel.grid=element_blank(),
+                  axis.title=element_blank())
+      
+      
+      map <- ggplot() +
+            geom_raster(data=dl, 
+                        aes(x, y), fill="white") +
+            geom_raster(data=filter(d, is.finite(land)), 
+                        aes(x, y, fill=ta#, alpha=tm
+                            )) +
+            scale_alpha_continuous(trans="log", range=0:1) +
+            scale_fill_gradientn(colors=rainbow(20)) +
+            scale_x_continuous(expand=c(0,0)) +
+            scale_y_continuous(expand=c(0,0)) +
+            theme_void() +
+            theme(legend.position="none",
+                  panel.background=element_rect(fill="black"))
+      png(paste0("figures/tailwinds/temperature_angle_s", neigh, ".png"), width=2000, height=1000)
+      plot(map)
+      plot(compass, vp=viewport(x=.13, y=.33, width=.25, height=.35))
+      dev.off()
+      
+      
+      
+      da <- dl %>%
+            mutate(tm=ifelse(tm > 1, 1, tm),
+                   tm=ifelse(tm < -.8, -.8, tm))
+      ld <- expand.grid(theta=seq(0, 180, length.out=20)-360/80, 
+                        velocity=seq(min(da$tm), max(da$tm), length.out=20))
+      pal <- c("dodgerblue", "blue", "purple", "red", "orange")
+      col_v0 <- "white"
+      tach <- ggplot(ld, aes(theta, velocity, 
+                             fill=theta, alpha=velocity)) +
+            geom_tile(fill=col_v0, alpha=1) +
+            geom_tile() +
+            annotate(geom="segment", 
+                     x=60, xend=60, y=min(da$tm), yend=max(da$tm)*.7,
+                     arrow=arrow(type="closed", angle=15), size=1) +
+            annotate(geom="segment", 
+                     x=59, xend=60, y=mean(da$tm), yend=mean(da$tm),
+                     arrow=arrow(type="closed", angle=15)) +
+            annotate(geom="segment", 
+                     x=1, xend=0, y=mean(da$tm), yend=mean(da$tm),
+                     arrow=arrow(type="closed", angle=15)) +
+            annotate(geom="line", size=1,
+                     x=0:60, y=mean(da$tm)) +
+            annotate(geom="text",
+                     x=c(30, 75), y=c(mean(da$tm)*2.2, 0),
+                     label=c(expression("Theta"), "v"), 
+                     size=14, fontface="bold", parse=T) +
+            scale_x_continuous(limits=c(0, 360),
+                               breaks=seq(0, 180, 45),
+                               labels = function(x) paste0(x, "°")) +
+            scale_fill_gradientn(colours=pal,
+                                 breaks=seq(0, 180, 45), limits=c(0, 180)) +
+            coord_polar() +
+            theme(legend.position="none",
+                  axis.title=element_blank(),
+                  axis.text.x=element_text(color="white", size=25),
+                  #axis.text.x=element_blank(),
+                  axis.text.y=element_blank(),
+                  axis.ticks=element_blank(),
+                  plot.background=element_blank(),
+                  panel.background=element_blank(),
+                  panel.grid=element_blank())
+      map <- ggplot() +
+            geom_raster(data=dl, 
+                        aes(x, y), fill=col_v0) +
+            geom_raster(data=da, 
+                        aes(x, y, fill=theta#, alpha=tm
+                            )) +
+            scale_fill_gradientn(colours=pal,
+                                 breaks=seq(0, 180, 45), limits=c(0, 180)) +
+            scale_x_continuous(expand=c(0,0)) +
+            scale_y_continuous(expand=c(0,0)) +
+            theme_void() +
+            theme(legend.position="none",
+                  panel.background=element_rect(fill="black"))
+      png(paste0("figures/tailwinds/alignment_s", neigh, ".png"), width=2000, height=1000)
+      plot(map)
+      plot(tach, vp=viewport(x=.04, y=.33, width=.3, height=.5))
+      grid.text("tailwind", hjust=0, x=.04, y=.58, gp=gpar(col=pal[1], cex=2.5, fontface="bold"))
+      grid.text("crosswind", hjust=0, x=.18, y=.335, gp=gpar(col=pal[3], cex=2.5, fontface="bold"))
+      grid.text("headwind", hjust=0, x=.04, y=.09, gp=gpar(col=pal[5], cex=2.5, fontface="bold"))
+      dev.off()
+}
 
-###########
 
-# project to hobo-dyer equal area
-s <- projectRaster(s, crs=CRS("+proj=cea +lon_0=0 +lat_ts=37.5 +x_0=0 +y_0=0 +ellps=WGS84 +units=m +no_defs"))
-
-#continents <- map_data("world")
-#states <- map_data("state")
-
-
-########################
-
-
-d <- s %>% 
-      rasterToPoints %>% 
-      as.data.frame()
-d <- d %>%
-      mutate(theta = abs(wa - ta),
-             theta = ifelse(theta > 180, 360-theta, theta))
-
-dl <- filter(d, is.finite(land))
-
-
-
-
-compass <- ggplot() +
-      geom_point(data=data.frame(x=0:360), 
-                 aes(x, .9, color=x),
-                 size=5) +
-      geom_segment(data=data.frame(x=seq(0, 360, 45)), 
-                   aes(x=x, xend=x, y=.5, yend=1.25, color=x),
-                   arrow=arrow(type="closed", angle=12,
-                               length=unit(0.5, "inches")),
-                   size=2) +
-      coord_polar() +
-      scale_x_continuous(breaks=seq(0, 360, 45),
-                         limits=c(0, 360)) +
-      scale_color_gradientn(colors=rainbow(20)) +
-      theme_minimal() +
-      theme(legend.position="none",
-            axis.text.y=element_blank(),
-            axis.text.x=element_blank(),
-            panel.grid=element_blank(),
-            axis.title=element_blank())
-
-
-map <- ggplot() +
-      geom_raster(data=dl, 
-                  aes(x, y), fill="white") +
-      geom_raster(data=filter(d, is.finite(land)), 
-                  aes(x, y, fill=ta, alpha=tm)) +
-      scale_alpha_continuous(trans="log", range=0:1) +
-      scale_fill_gradientn(colors=rainbow(20)) +
-      scale_x_continuous(expand=c(0,0)) +
-      scale_y_continuous(expand=c(0,0)) +
-      theme_void() +
-      theme(legend.position="none",
-            panel.background=element_rect(fill="black"))
-png("figures/temperature_direction.png", width=2000, height=1000)
-plot(map)
-plot(compass, vp=viewport(x=.13, y=.33, width=.25, height=.35))
-dev.off()
-
-
-
+###
 
 
 map <- ggplot() +
@@ -137,7 +228,7 @@ map <- ggplot() +
       theme_void() +
       theme(legend.position="none",
             panel.background=element_rect(fill="black"))
-png("figures/wind_direction.png", width=2000, height=1000)
+png("figures/tailwinds/wind_direction.png", width=2000, height=1000)
 plot(map)
 plot(compass, vp=viewport(x=.13, y=.33, width=.25, height=.35))
 dev.off()
@@ -154,7 +245,7 @@ p <- ggplot(dl %>% sample_n(10000),
       theme_minimal() +
       theme(legend.position="none") +
       labs("mean wind azimuth and magnitude (terrestrial only)")
-ggsave("figures/scatter_azimuth_magnitude.png", p, width=8, height=8, units="in")
+ggsave("figures/tailwinds/scatter_azimuth_magnitude.png", p, width=8, height=8, units="in")
 
 p <- ggplot(d %>% sample_n(10000), 
             aes(wa, wm, color=wa)) +
@@ -167,7 +258,7 @@ p <- ggplot(d %>% sample_n(10000),
       theme_minimal() +
       theme(legend.position="none") +
       labs("mean wind azimuth and magnitude (terrestrial and ocean)")
-ggsave("figures/scatter_azimuth_magnitude_all.png", p, width=8, height=8, units="in")
+ggsave("figures/tailwinds/scatter_azimuth_magnitude_all.png", p, width=8, height=8, units="in")
 
 
 map <- ggplot() +
@@ -182,72 +273,9 @@ map <- ggplot() +
       scale_y_continuous(expand=c(0,0), limits=c(-80, NA)) +
       theme_void() +
       theme(legend.position="none")
-png("figures/wind_direction_full.png", width=2000, height=1000)
+png("figures/tailwinds/wind_direction_full.png", width=2000, height=1000)
 plot(map)
 plot(compass, vp=viewport(x=.13, y=.33, width=.25, height=.35))
-dev.off()
-
-
-
-da <- dl %>%
-      mutate(tm=ifelse(tm > 1, 1, tm),
-             tm=ifelse(tm < -.8, -.8, tm))
-ld <- expand.grid(theta=seq(0, 180, length.out=20)-360/80, 
-                  velocity=seq(min(da$tm), max(da$tm), length.out=20))
-pal <- c("dodgerblue", "blue", "purple", "red", "orange")
-col_v0 <- "white"
-tach <- ggplot(ld, aes(theta, velocity, 
-               fill=theta, alpha=velocity)) +
-      geom_tile(fill=col_v0, alpha=1) +
-      geom_tile() +
-      annotate(geom="segment", 
-               x=60, xend=60, y=min(da$tm), yend=max(da$tm)*.7,
-               arrow=arrow(type="closed", angle=15), size=1) +
-      annotate(geom="segment", 
-               x=59, xend=60, y=mean(da$tm), yend=mean(da$tm),
-               arrow=arrow(type="closed", angle=15)) +
-      annotate(geom="segment", 
-               x=1, xend=0, y=mean(da$tm), yend=mean(da$tm),
-               arrow=arrow(type="closed", angle=15)) +
-      annotate(geom="line", size=1,
-               x=0:60, y=mean(da$tm)) +
-      annotate(geom="text",
-               x=c(30, 75), y=c(mean(da$tm)*2.2, 0),
-               label=c(expression("Theta"), "v"), 
-               size=14, fontface="bold", parse=T) +
-      scale_x_continuous(limits=c(0, 360),
-                         breaks=seq(0, 180, 45),
-                         labels = function(x) paste0(x, "°")) +
-      scale_fill_gradientn(colours=pal,
-                           breaks=seq(0, 180, 45), limits=c(0, 180)) +
-      coord_polar() +
-      theme(legend.position="none",
-            axis.title=element_blank(),
-            axis.text.x=element_text(color="white", size=25),
-            #axis.text.x=element_blank(),
-            axis.text.y=element_blank(),
-            axis.ticks=element_blank(),
-            plot.background=element_blank(),
-            panel.background=element_blank(),
-            panel.grid=element_blank())
-map <- ggplot() +
-      geom_raster(data=dl, 
-                  aes(x, y), fill=col_v0) +
-      geom_raster(data=da, 
-                  aes(x, y, fill=theta, alpha=tm)) +
-      scale_fill_gradientn(colours=pal,
-                           breaks=seq(0, 180, 45), limits=c(0, 180)) +
-      scale_x_continuous(expand=c(0,0)) +
-      scale_y_continuous(expand=c(0,0)) +
-      theme_void() +
-      theme(legend.position="none",
-            panel.background=element_rect(fill="black"))
-png("figures/alignment.png", width=2000, height=1000)
-plot(map)
-plot(tach, vp=viewport(x=.04, y=.33, width=.3, height=.5))
-grid.text("tailwind", hjust=0, x=.04, y=.58, gp=gpar(col=pal[1], cex=2.5, fontface="bold"))
-grid.text("crosswind", hjust=0, x=.18, y=.335, gp=gpar(col=pal[3], cex=2.5, fontface="bold"))
-grid.text("headwind", hjust=0, x=.04, y=.09, gp=gpar(col=pal[5], cex=2.5, fontface="bold"))
 dev.off()
 
 
@@ -319,15 +347,15 @@ p <- ggplot(dl, aes(theta)) +
       scale_x_continuous(breaks=seq(0, 180, 45)) +
       scale_y_continuous(breaks=seq(0, 1, .1)) +
       theme_minimal()
-ggsave("figures/ecdf_theta.png", p, width=8, height=8, units="in")
+ggsave("figures/tailwinds/ecdf_theta.png", p, width=8, height=8, units="in")
 
 
 p <- ggplot(dl, 
-       aes(y, theta)) +
+            aes(y, theta)) +
       geom_point(size=.05) + 
       geom_smooth(se=F, color="red") +
       scale_y_continuous(breaks=seq(0, 180, 45), expand=c(0,0)) +
       scale_x_continuous(breaks=c(-90, -67, -23.5, 0, 23.5, 67, 90), expand=c(0,0)) +
       theme_minimal() +
       labs(x="latitude")
-ggsave("figures/scater_latitude_theta.png", p, width=8, height=8, units="in")
+ggsave("figures/tailwinds/scater_latitude_theta.png", p, width=8, height=8, units="in")
