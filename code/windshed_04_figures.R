8f89e34ce46a90642eed9b64e0e617d6ad301846
Matthew Kling <mattkling@gmail.com>
2019-12-04 18:09:37 -0800

    further syndrome plots

--- code/windshed_04_figures.R
+++ code/windshed_04_figures.R
@@ -615,8 +615,97 @@ rglplots <- function(){
     snapshot3d(paste0("figures/windsheds/rgl/persp_", drn, ".png"))
     rgl.quit()
   }
+  
+  
+  ###############
+  
+  for(drn in unique(d$direction)){
+    
+    d <- f %>%
+      filter(direction == drn,
+             moment == "windshed",
+             stat == "size") %>%
+      spread(property, value) %>%
+      mutate(windfill = overlap / clim)
+    d <- f %>%
+      filter(direction == drn,
+             moment == "windshed",
+             property == "wind",
+             stat == "isotropy") %>%
+      rename(isotropy = value) %>%
+      select(x, y, isotropy) %>%
+      left_join(d, .) %>%
+      
+      filter(is.finite(windfill),
+             is.finite(clim),
+             is.finite(isotropy)) %>%
+      mutate(isotropy=truncate(isotropy),
+             windfill = truncate(windfill, .001, c("high", "low"))) %>%
+      
+      left_join(areas) %>%
+      arrange(clim) %>%
+      mutate(climrnk = cumsum(area)) %>%
+      arrange(windfill) %>%
+      mutate(windfillrnk = cumsum(area)) %>%
+      arrange(isotropy) %>%
+      mutate(isotropyrnk = cumsum(area)) %>%
+      
+      mutate(syndrome = case_when(climrnk < mean(climrnk) ~ "climate-limited",
+                                  windfillrnk > mean(windfillrnk) ~ "wind-facilitated",
+                                  isotropyrnk > mean(isotropyrnk) ~ "speed-hindered",
+                                  TRUE ~ "direction-hindered"))
+    
+    palette <- c("white", "red", "gold", "black")
+    d <- d %>% mutate(color = colors2d(cbind(.$isotropyrnk, .$windfillrnk),
+                                       palette[c(4,3,2,4)]))
+    
+    cst <- d %>%
+      filter(x >= ext@xmin, x <= ext@xmax, y >= ext@ymin, y <= ext@ymax)
+    cst <- expand.grid(x=unique(cst$x), y=unique(cst$y)) %>%
+      left_join(cst) %>%
+      mutate(color=ifelse(is.na(color), "white", color)) %>%
+      arrange(-y, x)
+    
+    dem <- stack("data/geographic/processed/elevation.tif") %>% crop(ext)
+    #wind_color <- matrix(cst$color, nrow=nrow(dem), byrow=T)
+    crgb <- col2rgb(cst$color)
+    
+    crgb <- rbind(crgb, cst$climrnk/max(cst$climrnk, na.rm=T)) %>%
+      apply(2, function(x){if(any(is.na(x))) return(rep(255, 3))
+                          return(255 - ((255 - x[1:3]) * x[4]))})
+    
+    clr <- stack(dem, dem, dem)
+    clr[[1]][] <- crgb[1,]
+    clr[[2]][] <- crgb[2,]
+    clr[[3]][] <- crgb[3,]
+    clr <- resample(clr, elev)
+    clr <- trim(clr) %>% reclassify(c(-Inf, 0, 0, 255, Inf, 255))
+    elev <- crop(elev, clr)
+    
+    clr <- rgb(values(clr), maxColorValue = 255)
+    wind_color <- matrix(clr, nrow=nrow(elev), byrow=T) %>% t()
+    demm <- matrix(values(elev), nrow=nrow(elev), byrow=T) %>% t()
+    wind_color[demm<=0] <- "white"
+    
+    z <- demm
+    x <- -1000 * (1:nrow(z))
+    y <- 1000 * (1:ncol(z))
+    
+    library(rgl)
+    open3d()
+    rgl.surface(x, -y, z*12, col=wind_color, shininess=128)
+    rgl.viewpoint(theta = 180, phi = 45, zoom=.17)
+    rgl.clear("lights")
+    light3d(phi=15, specular="gray60")
+    par3d("windowRect" = c(0,0,1000,1000))
+    snapshot3d(paste0("figures/windsheds/rgl/syndrome_", drn, ".png"))
+    rgl.quit()
+  }
+
 }
 rglplots()
+
+
 
 ### climate vs windfilling
 
@@ -822,17 +911,17 @@ for(drn in c("inbound", "outbound")){
     scale_y_continuous(expand=c(0,0), limits=c(-90, 90)) +
     theme_void() +
     theme(legend.position=c(.15, .3),
-          legend.title = element_blank(),
           legend.text = element_text(color="black"),
-          panel.background = element_rect(fill="gray75"))
+          panel.background = element_rect(fill="gray75")) +
+    labs(fill = drn)
   ggsave(paste0("figures/windsheds/global/syndrome_", drn, ".png"), 
-      width=8, height=4, units="in")
+         width=8, height=4, units="in")
   
   
   d <- d %>% mutate(color = colors2d(cbind(.$isotropyrnk, .$windfillrnk),
                                      palette[c(4,3,2,4)]))
   
-  p <- ggplot(d, aes(x, y, alpha=rank(clim), fill=syndrome)) +
+  p <- ggplot(d, aes(x, y, alpha=climrnk, fill=syndrome)) +
     geom_raster() +
     geom_raster(fill=palette[1], alpha=1) + # color for climate-limited
     geom_raster(fill=d$color) +
@@ -842,13 +931,11 @@ for(drn in c("inbound", "outbound")){
     scale_y_continuous(expand=c(0,0), limits=c(-90, 90)) +
     theme_void() +
     theme(legend.position=c(.15, .3),
-          legend.title = element_blank(),
           legend.text = element_text(color="black"),
-          panel.background = element_rect(fill="gray75"))
-  p
+          panel.background = element_rect(fill="gray75")) +
+    labs(fill = drn)
   ggsave(paste0("figures/windsheds/global/syndrome_cont_", drn, ".png"), 
          width=8, height=4, units="in")
-  
 }
 
 
